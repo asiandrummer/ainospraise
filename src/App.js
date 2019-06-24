548b9934958972ff3055cd9d7f9273d6af183075
Hyo Jeong <asiandrummer@fb.com>
2019-06-24 15:31:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Cell, Grid, Row } from "@material/react-layout-grid";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video-container">
+      <video loop autoPlay muted playsInline>
+        <source src="/video/jus.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
