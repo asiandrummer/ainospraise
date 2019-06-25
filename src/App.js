be29e89ec74cfd36bb01ce1e0d033dcf7975ad93
Hyo Jeong <asiandrummer@fb.com>
2019-06-24 17:01:17 -0700

    [layout] create some bs layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Cell, Grid, Row } from "@material/react-layout-grid";
+import { Grid, Row, Cell } from "@material/react-layout-grid";
+import Button from "@material/react-button";
 
 import "./App.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className="video-container">
-      <video loop autoPlay muted playsInline>
-        <source src="/video/jus.mp4" type="video/mp4" />
-      </video>
+    <div className="App">
+      <div className="video-container">
+        <video loop autoPlay muted playsInline>
+          <source src="/video/jus.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="content">
+        <Grid>
+          <Row>
+            <Cell desktopColumns={4} phoneColumns={4} tabletColumns={2} />
+            <Cell desktopColumns={4} phoneColumns={4} tabletColumns={4}>
+              <h1 className="team-name">Ainos</h1>
+              <h1 className="team-name">Praise</h1>
+              <Button outlined>Join Our Team</Button>
+            </Cell>
+            <Cell desktopColumns={4} phoneColumns={4} tabletColumns={2} />
+          </Row>
+        </Grid>
+      </div>
     </div>
   );
 }
-
-export default App;
