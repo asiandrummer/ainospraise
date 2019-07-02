fa513d6a1cc937a85d6cbd88b07391c07ab5954b
Hyo Jeong <asiandrummer@fb.com>
2019-07-02 15:48:19 -0700

    Updates

---
+++ precache-manifest.7c192374012cdb217ea2a30cb453886e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11cfa4c8acad60ea6f6210f5896a81d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06df99c35a20ea5529f9",
+    "url": "/static/css/main.9b805c36.chunk.css"
+  },
+  {
+    "revision": "cb36f31bb0ca2e2f9b7a",
+    "url": "/static/js/2.f90342be.chunk.js"
+  },
+  {
+    "revision": "06df99c35a20ea5529f9",
+    "url": "/static/js/main.16099b03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ainosworship/precache-manifest.f262331ae06e84f843cf27be13796253.js"
+  "/precache-manifest.7c192374012cdb217ea2a30cb453886e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ainosworship/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
